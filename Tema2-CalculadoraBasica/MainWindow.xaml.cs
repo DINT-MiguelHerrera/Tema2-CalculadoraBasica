b8f66dd4234bcd50674e5f4e2da9f5c1cac298ba
alumno <alumno@A302PC35>
2023-10-04 13:45:59 +0200

    Calculadora Básica Terminada

--- Tema2-CalculadoraBasica/MainWindow.xaml.cs
+++ Tema2-CalculadoraBasica/MainWindow.xaml.cs
@@ -23,6 +23,70 @@ namespace Tema2_CalculadoraBasica
         public MainWindow()
         {
             InitializeComponent();
+            botonCalcular.IsEnabled = false;
+        }
+        private void BotonCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double operando1 = double.Parse(casillaOperando1.Text);
+                double operando2 = double.Parse(casillaOperando2.Text);
+                char operador = char.Parse(casillaOperador.Text);
+
+                switch (operador)
+                {
+                    case '+':
+                        casillaResultado.Text = (operando1 + operando2).ToString();
+
+                        break;
+                    case '-':
+                        casillaResultado.Text = (operando1 - operando2).ToString();
+
+                        break;
+                    case '*':
+                        casillaResultado.Text = (operando1 * operando2).ToString();
+
+                        break;
+                    case '/':
+                        casillaResultado.Text = (operando1 / operando2).ToString();
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha producido un error. Revise los operandos y el operador");
+            }
+        }
+
+        private void BotonLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            casillaOperando1.Text = "";
+            casillaOperando2.Text = "";
+            casillaOperador.Text = "";
+            casillaResultado.Text = "";
+        }
+
+        private void casillaOperador_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            switch (casillaOperador.Text)
+            {
+                case "+":
+                    botonCalcular.IsEnabled = true;
+                    break;
+                case "-":
+                    botonCalcular.IsEnabled = true;
+                    break;
+                case "*":
+                    botonCalcular.IsEnabled = true;
+                    break;
+                case "/":
+                    botonCalcular.IsEnabled = true;
+                    break;
+                default:
+                    botonCalcular.IsEnabled = false;
+                    break;
+            }
         }
     }
 }
